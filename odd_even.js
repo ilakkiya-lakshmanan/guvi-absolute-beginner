8253ac73dcdd79dbc1faa881dc693070098e7da6
ilakkiya-lakshmanan <ilakkiyalakshmanan@gmail.com>
2020-06-16 20:06:10 +0530

    added code for odd even program

---
+++ odd_even.js
@@ -0,0 +1,15 @@
+let userInput=0.00001;
+  if(parseInt(userInput) == 0)
+  {
+    console.log("Zero");
+  }
+else if((Math.round(userInput)%2)==0)
+{
+  console.log("Even");
+}
+else
+{
+    console.log("Odd");
+}
+
+
